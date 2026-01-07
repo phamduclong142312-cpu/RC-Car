90340f51ef94b9a5a1c41869503094155d20b4bf
phamduclong142312-cpu <phamduclong142312@gmail.com>
2026-01-07 20:24:51 +0700

    Update TayCamXeRC.cpp

--- TayCamXeRC.cpp
+++ TayCamXeRC.cpp
@@ -1,11 +1,20 @@
 #include <WiFi.h>
 #include <esp_now.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define potDanhlai 35
-#define potSpeedDC 34
-#define potMaxDC 33
-#define potSteeringTrim 32
-#define Congtac 36
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); //khai báo màn hình
+
+#define potDanhlai 35 //để tạm
+#define potSpeedDC 34 //để tạm
+#define potMaxDC 33 //để tạm
+#define potSteeringTrim 32 //để tạm
+#define Congtac 36 //để tạm
 uint8_t receiverAddr[] = {0xcc, 0x7b, 0x5c, 0x25, 0xbf, 0x20};
 
 typedef struct struct_message{
@@ -13,7 +22,7 @@ typedef struct struct_message{
   int Steering; //0-180 (góc quay servo)
   int Speed;  //-100 - 100 (âm lùi, dương tiến)
   int SpeedLimit;//0-100
-  int ServoTrim; // căn chỉnh servo để xe đi thẳng chắc vậy
+  int ServoTrim; // căn chỉnh servo để xe đi thẳng chắc vậy -15-15 
 } struct_message;
 
 struct_message DataToSend;
@@ -27,7 +36,7 @@ void setup() {
     return;
   }
 
-  esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, receiverAddr, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
@@ -36,6 +45,14 @@ void setup() {
     Serial.println("Khong the bat cap");
     return;
   }
+
+  Wire.begin(21, 22);
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("Khong tim thay OLED");
+    while (true);
+  }
+  oled.setTextColor(SSD1306_WHITE); //pixel hiện sẽ có màu trắng
+  oled.clearDisplay(); // xóa hết màn hình
 }
 
 void loop() {
@@ -50,4 +67,32 @@ void loop() {
   DataToSend.ServoTrim = map(value4, 0, 4095, -15, 15);
   esp_now_send(receiverAddr, (uint8_t *) &DataToSend, sizeof(DataToSend));
   delay(100);
+
+  oled.clearDisplay();
+  oled.setTextSize(1); //chỉnh size chữ gốc 5x7 pixels
+
+  if(DataToSend.button == HIGH){
+    oled.setCursor(4,4);
+    oled.print("TU HANH");
+  }
+  else{
+    oled.setCursor(4,3);
+    oled.print("SPEED : ");
+    oled.print(DataToSend.Speed);
+
+    oled.setCursor(4,12);
+    oled.print("SERVO ANGLE : ");
+    oled.print(DataToSend.Steering);
+
+    oled.setCursor(4,21);
+    oled.print("MAX SPEED : ");
+    oled.print(DataToSend.SpeedLimit);
+
+    oled.setCursor(4,30);
+    oled.print("SERVO TRIM : ");
+    oled.print(DataToSend.ServoTrim);
+
+  }
+
+  oled.display(); // bắt buộc để oled hiện được chữ
 }
